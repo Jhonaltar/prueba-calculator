5b823d0b925a2257e92f8cf3b68c1b20e86ad193
jhonaltar <jhon96alta@gmail.com>
2026-07-04 10:54:58 -0500

    feat: primer carga

---
+++ src/calculator.py
@@ -0,0 +1,10 @@
+def sumar(a, b):
+    """Suma dos números."""
+    return a + b
+
+def restar(a, b):
+    """Resta dos números."""
+    return a - b
+
+print(sumar(5, 3))  # Output: 8
+print(restar(5, 3))  # Output: 2
